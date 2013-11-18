57fc359f3b1565b699f6ab0a5cb27f643da9494d
nathanallen <thenathanator@gmail.com>
2013-11-17 16:49:43 -0800

    clean up and refactor comments

--- bigram_counter.rb
+++ bigram_counter.rb
@@ -1,34 +1,27 @@
-# use default file unless user specifies something else
-file_name = ARGV.first || 'input.txt'
-
-# basic regex patterns
-word_pattern = /\w+/
-bigram_pattern = /\w+ \w+/
-
 # read in text file
+file_name = ARGV.first || 'input.txt'
 text = File.read(file_name).downcase
 
-# offset text by one word
-offset_text = text.match(word_pattern).post_match	
+# basic regex patterns (these need to be more precise)
+word_pattern = /[a-z]+/
+bigram_pattern = /[a-z]+ [a-z]+/
 
-# find all pairs, sort alphabetically
+# find even pairs
 even_pairs = text.scan(bigram_pattern)
+
+# find odd pairs
+offset_text = text.match(word_pattern).post_match	
 odd_pairs = offset_text.scan(bigram_pattern)
-all_pairs = (even_pairs + odd_pairs).sort
 
 # find unique pairs
-bigrams = all_pairs.uniq
+all_pairs = even_pairs + odd_pairs
+bigrams = all_pairs.sort.uniq
 
-# count frequency of pairs, sort by frequency descending
+# count frequency of pairs and sort by frequency descending
 bigram_frequency_hash = {}
-
 bigrams.each do |bigram|
-	p bigram
-	bigram_frequency_hash[bigram] = all_pairs.count(bigram)
+  bigram_frequency_hash[bigram] = all_pairs.count(bigram)
 end.sort_by {|k, v| v}.reverse
 
 # return ten most frequent pairs
 p bigram_frequency_hash.take(10)
-
-
-# h = h.sort_by() {|k, v| v}
