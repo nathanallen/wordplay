da70a34123f9797f05326a7c36647883162e2563
nathanallen <thenathanator@gmail.com>
2013-11-19 15:23:24 -0800

    WIP: refactoring

--- numbers_in_words.rb
+++ numbers_in_words.rb
@@ -7,51 +7,63 @@ NUM_WORDS = {
   1000=>"thousand", 1000000=>"million", 1000000000=>"billion", 1000000000000=>"trillion"
 }
 
+def in_words(n)
+  words = []
+  a = n.to_delimited_array
+
+  while a.length > 0
+    unit = calculate_unit(a)
+    nums = a.slice!(0)
+    hundreds_place = nums[-3]
+    tens_place = nums[-2]
+    ones_place = nums[-1]
+   
+    if (1..9).include?(hundreds_place)
+      words << NUM_WORDS[hundreds_place]
+      words << NUM_WORDS[100]
+      if tens_place == 0 && ones_place != 0
+        words << "and"
+      end
+    end
+
+    if tens_place == 1
+      teens = [tens_place, ones_place].join.to_i
+      words << NUM_WORDS[teens]
+    elsif (2..9).include?(tens_place)
+      words << NUM_WORDS[tens_place*10]
+      words << NUM_WORDS[ones_place] unless ones_place == 0
+    elsif (1..9).include?(ones_place)
+      words << NUM_WORDS[ones_place]
+    end
+
+    words << NUM_WORDS[unit] if unit
+  end
+
+  return NUM_WORDS[n] if n == 0
+  return output.join(' ')
+end
+
 class Integer
   def to_a
     to_s.chars.map { |n| n.to_i }
   end
 
-  def three_by_three
-    output = []
-    n_array = self.to_a
-    remainder = n_array.length%3
-    output << n_array.slice!(0,remainder) if remainder != 0
-    output << n_array.slice!(0,3) while n_array.length > 0
-    output
+  def to_delimited_array
+    nums = self.to_a
+    length_of_first_set = nums.length%3
+    a = []
+    a << nums.slice!(0,length_of_first_set) if length_of_first_set != 0
+    a << nums.slice!(0,3) while nums.length > 0
+    a
   end
 end
 
-
-def in_words(n)
-  output = []
-  n_array = n.three_by_three
-  while n_array.length > 0
-    unit = nil
-    unit = NUM_WORDS[10**(n_array.length+1)] unless n_array.length == 1
-    current_chunk = n_array.slice!(0)
-    if current_chunk[-3] != nil && current_chunk[-3] != 0
-# p "1st"
-      output << NUM_WORDS[current_chunk[-3]]
-      output << NUM_WORDS[100]
-    end
-    if current_chunk[-2] == (1 || 2) 
-# p "2nd"
-      output << NUM_WORDS[current_chunk[-2..-1].join.to_i]
-    elsif current_chunk[-2] != nil && current_chunk[-2] >= 2 
-# p "3rd"
-      output << NUM_WORDS[current_chunk[-2]*10]
-      output << NUM_WORDS[current_chunk[-1]] unless current_chunk[-1] == 0
-    elsif current_chunk[-1] != nil
-# p "4th"
-      output << NUM_WORDS[current_chunk[-1]] unless current_chunk[-1] == 0
-    end
-      output << unit if unit
-  end
-#p output
-return output.join(' ')
+def calculate_unit(delimited_array)
+ 10**((delimited_array.length-1)*3) unless delimited_array.length == 1
 end
 
+__END__
+#Test code:
 puts in_words(0) == "zero"
 puts in_words(1) == "one"
 puts in_words(10) == "ten"
@@ -59,20 +71,16 @@ puts in_words(19) == "nineteen"
 puts in_words(20) == "twenty"
 puts in_words(21) == "twenty one"
 puts in_words(100) == "one hundred"
-puts in_words(101) == "one hundred one"
+puts in_words(101) == "one hundred and one"
 puts in_words(110) == "one hundred ten"
 puts in_words(1000) == "one thousand"
 puts in_words(10000) == "ten thousand"
-puts in_words(11101) == "eleven thousand one hundred one"
+puts in_words(11101) == "eleven thousand one hundred and one"
 puts in_words(100000) == "one hundred thousand"
+puts in_words(1_111_111_111) == "one billion one hundred eleven million one hundred eleven thousand one hundred eleven"
 
-
-__END__
-##Test code:
-
-## three_by_three method:
-# puts 1.three_by_three == [[1]]
-# puts 10.three_by_three == [[1,0]]
-# puts 100.three_by_three == [[1,0,0]]
-# puts 1000.three_by_three == [[1],[0,0,0]]
-# puts 1000000.three_by_three == [[1],[0,0,0],[0,0,0]]
+puts 1.to_delimited_array == [[1]]
+puts 10.to_delimited_array == [[1,0]]
+puts 100.to_delimited_array == [[1,0,0]]
+puts 1000.to_delimited_array == [[1],[0,0,0]]
+puts 1000000.to_delimited_array == [[1],[0,0,0],[0,0,0]]
