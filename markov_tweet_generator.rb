b774d0a99c3f1aa2a6ed38e9cd38f395592da937
nathanallen <thenathanator@gmail.com>
2014-01-26 23:14:36 -0800

    reorganization & naming of things

--- markov_tweet_generator.rb
+++ markov_tweet_generator.rb
@@ -1,39 +1,56 @@
 class TweetRegurgitator
   require 'CSV'
 
-  attr_reader :word_p
+  attr_reader :word_probs, :word_freqs
 
   def initialize(tweets_csv)
     calculate_frequencies_from_file(tweets_csv)
-    @word_p = calculate_probabilities
+    @word_probs = calculate_probabilities
   end
 
-  def calculate_frequencies_from_file(tweets_csv)
-    tweet_rows = CSV.parse(File.read('tweets.csv'))
-    parse_tweets(tweet_rows)
+  def generate_tweet
+    generate_tweets
   end
 
-  def word_freqs
-    @word_freqs ||= {}
-  end
-
-  def parse_tweets(tweet_rows)
-    for row in 1..(tweet_rows.length-1)
-      words_in_tweet = tweet_rows[row][5].split(' ')
-      words_in_tweet.each_index do |word_index|
-        current_word = words_in_tweet[word_index]
-        next_word = words_in_tweet[word_index + 1]
-        update_word_frequency(current_word, next_word) unless next_word.nil?
-      end
+  def generate_tweets(n=1)
+    n.times.map do
+      words = generate_word_branch(random_seed_word)
+      format(words)
     end
   end
 
-  def update_word_frequency(current_word, next_word)
-    nest_hash = word_freqs[current_word]
-    if nest_hash && nest_hash[next_word]
-      word_freqs[current_word][next_word] += 1
+  private
+
+  def calculate_frequencies_from_file(tweets_csv)
+    csv_rows = CSV.parse(File.read(tweets_csv))
+    parse_tweets(csv_rows)
+  end
+
+  def parse_tweets(csv_rows)
+    for row in 1..(csv_rows.length-1)
+      words_in_tweet = csv_rows[row][5].split(' ')
+      find_word_pairs_and_update_freqs(words_in_tweet)
+    end
+  end
+
+  def find_word_pairs_and_update_freqs(words_in_tweet)
+    words_in_tweet.each_index do |word_index|
+      current_word = words_in_tweet[word_index]
+      next_word = words_in_tweet[word_index + 1]
+      update_frequency(current_word, next_word) unless next_word.nil?
+    end
+  end
+
+  def frequency_of
+    @word_freqs ||= {}
+  end
+
+  def update_frequency(current_word, next_word)
+    nested_hash = frequency_of[current_word]
+    if nested_hash && nested_hash[next_word]
+      frequency_of[current_word][next_word] += 1
     else
-      word_freqs[current_word] = {next_word => 1}
+      frequency_of[current_word] = {next_word => 1}
     end
   end
 
@@ -47,33 +64,31 @@ class TweetRegurgitator
   end
 
   def random_seed_word
-    @word_p.keys.sample
+    word_probs.keys.sample
   end
 
   def random_percent
     rand(0..1.0)
   end
+ 
+  def generate_word_branch(*words)
+    until words[-1].nil? || words.length == 23 # sanity check against infinite loop
+      seed_word = words[-1]
+      seed_hash = word_probs[seed_word]
+      words << next_word_or_nil(seed_hash, random_percent)
+    end
+    words[0..-2]
+  end
 
-  def next_word(seed_word, goal_v, current_v=0)
-    word_p[seed_word].take_while do |word_prob|
+  def next_word_or_nil(seed_hash, random_percent)
+    seed_hash ? next_word_by_probability(seed_hash, random_percent) : nil
+  end
+
+  def next_word_by_probability(seed_hash, goal_v, current_v=0)
+    seed_hash.take_while do |word_prob|
       current_v += word_prob.last
       current_v >= goal_v
     end.first.first
-  end
-
-  def generate_tweets(n=1)
-    n.times.map do
-      words = chain_together_words(random_seed_word)
-      format(words)
-    end
-  end
- 
-  def chain_together_words(*words)
-    until words[-1].nil? || words.length == 23 # assumes average word length of 5 characters
-      seed_word = words[-1]
-      words << ( word_p[seed_word] ? next_word(seed_word, random_percent) : nil )
-    end
-    words[0..-2]
   end
 
   def format(words)
